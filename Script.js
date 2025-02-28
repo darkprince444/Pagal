e2bd79af588bf1642a51bc595dacf238b4ae9632
darkprince444 <darkprince8984@gmail.com>
2025-02-28 06:54:51 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,16 @@
 
+
+document.getElementById("yes_btn").addEventListener("click", function() {
+    alert("Thank you my dear pagal ❤️ Love you love you love you so much 😘😘😘😘 ଆଜି ଠାରୁ ମୁଁ ଆଉ କେବେ ବି ଏମିତି କରିବିନି");
+});
+
+let button = document.getElementById("randomMoveButton");
+
+button.addEventListener("click", function() {
+    let x = Math.random() * (window.innerWidth - this.clientWidth);
+    let y = Math.random() * (window.innerHeight - this.clientHeight);
+
+    this.style.position = "absolute";
+    this.style.left = `${x}px`;
+    this.style.top = `${y}px`;
+});
